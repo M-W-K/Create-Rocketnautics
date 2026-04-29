c880d66ffab60000135d6f364971aa7dcc472533
M-W-K <31022105+M-W-K@users.noreply.github.com>
2026-04-29 01:04:58 -0600

    Fix loss of licensing

--- src/main/java/dev/devce/rocketnautics/SkyDataHandler.java
+++ src/main/java/dev/devce/rocketnautics/SkyDataHandler.java
@@ -1,9 +1,24 @@
+/*
+ * This file is part of Cosmonautics.
+ * Cosmonautics is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cosmonautics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cosmonautics.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package dev.devce.rocketnautics;
 
 import dev.devce.rocketnautics.content.physics.SpaceTransitionHandler;
 import dev.devce.rocketnautics.network.PlanetMapPayload;
 import it.unimi.dsi.fastutil.doubles.DoubleObjectPair;
-import it.unimi.dsi.fastutil.ints.IntObjectPair;
 import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
